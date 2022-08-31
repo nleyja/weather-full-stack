9e7f268da22da02d3b2371a43385e92ea4dc63d8
Noah <nleyja@gmail.com>
2022-08-31 16:42:29 -0500

    added project, wored on fetching api and search option, basic scss styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
+import WeatherDisplay from './components/SearchBar/WeatherDisplay';
+
+
 function App() {
+
+  const [ weather, setWeather] =useState()
+
+    const getWeather = (city) => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dd1f22729598cb482d2d5c16faf96c1d`)
+    .then((res)=> {
+      console.log(res);
+      return res.json()
+    })
+    .then((data) => {
+      console.log("data", data);
+      const errorMessage = data.message;
+      if(errorMessage){
+        console.log("City not found");
+      } else {
+        setWeather(data)
+        console.log(weather);
+      }
+      
+    })
+  }
+
+  // useEffect (getWeather,[])
+ // happens on component mount
+
+ const getFormattedWord = (rawWord) => {
+  let formattedWord = rawWord.toLowerCase();
+  formattedWord = formattedWord.charAt(0).toUpperCase() + formattedWord.slice(1) // returns word with first letter capitalized
+  return formattedWord
+ }
+
+
+ const handleButtonSearch = (event) => {
+  let rawInput = inputBox.current.value; // gets rid of extra spaces
+  while(rawInput.includes("  ")){
+    rawInput = rawInput.replaceAll("  ", " ")
+  }
+
+  const rawWords = rawInput.split(" ")
+  const formattedWords = rawWords.map(word => {
+    return getFormattedWord(word)  // spiltting by spaces creating an array of words
+  })
+
+  const formattedInput = formattedWords.join(" ") // takes the array and joins words with a space inbetween
+  console.log(formattedInput);
+  getWeather(formattedInput)
+ }
+
+ 
+ const inputBox = useRef() // creates reference to HTML element
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='weather__header'>
+        <h1>Weather App</h1>
+      {weather && <WeatherDisplay weather={weather}/>}
+      <input ref={inputBox} className='input__box'></input>
+      <button className='search__button' onClick={handleButtonSearch}>Search</button>
       </header>
     </div>
   );
---
+++ src/components/SearchBar/WeatherDisplay.jsx
@@ -0,0 +1,18 @@
+
+
+const WeatherDisplay = (props) => {
+  const { weather } = props;
+  return(
+    <div>
+      <h2>Today's weather in...</h2>
+      <h2>{weather.name}</h2>
+      <h3>{weather.sys.country}</h3>
+      <h3>{weather.weather[0].description}</h3>
+
+    </div>
+
+
+  )
+};
+
+export default WeatherDisplay;
